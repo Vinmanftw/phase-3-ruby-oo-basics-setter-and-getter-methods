80f5cf0e8f7dbaafc42e049a68c5b787212c660b
Vincent <vao2002@gmail.com>
2021-08-31 11:53:04 -0500

    commit tree f46e8145eccfec28143bdea0e082be7741c21b26

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+    
+    # getter method
+    def job
+        @job
+    end
+    
+    # getter method
+    def name
+        @name
+    end
 end
